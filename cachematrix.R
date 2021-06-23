6b00be463fa51fde2fc8b017fe9281e27bdf53fe
paolomiguel01 <86356652+paolomiguel01@users.noreply.github.com>
2021-06-23 21:26:54 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,62 +1,25 @@
-## A pair of functions that cache the inverse of a matrix
-
-
-## Creates a special matrix object that can cache its inverse
-makeCacheMatrix <- function( m = matrix() ) {
-
-    ## Initialize the inverse property
-    i <- NULL
-
-    ## Method to set the matrix
-    set <- function( matrix ) {
-            m <<- matrix
-            i <<- NULL
-    }
-
-    ## Method the get the matrix
-    get <- function() {
-        ## Return the matrix
-        m
-    }
-
-    ## Method to set the inverse of the matrix
-    setInverse <- function(inverse) {
-        i <<- inverse
-    }
-
-    ## Method to get the inverse of the matrix
-    getInverse <- function() {
-        ## Return the inverse property
-        i
-    }
-
-    ## Return a list of the methods
-    list(set = set, get = get,
-         setInverse = setInverse,
-         getInverse = getInverse)
+makeCacheMatrix <- function(z = matrix()) {
+  Dell <- NULL
+  set <- function(x) {
+    z <<- x
+    Dell <<- NULL
+  }  
+  get <- function() z
+  setmean <- function(mean) Dell <<- inverse
+  getmean <- function() Dell
+  list (set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
-
-
-cacheSolve <- function(x, ...) {
-
-    m <- x$getInverse()
-
- 
-    if( !is.null(m) ) {
-            message("getting cached data")
-            return(m)
-    }
-
-    ## Get the matrix from our object
-    data <- x$get()
-
-    ## Calculate the inverse using matrix multiplication
-    m <- solve(data) %*% data
-
-    ## Set the inverse to the object
-    x$setInverse(m)
-
-    ## Return the matrix
-    m
-}
+cacheSolve <- function(z, ...) {
+  Dell <- z$getinverse()
+  if (!is.null(Dell)) {
+    message("getting cached data")
+    return(Dell)
+  }
+  data <- z$get()
+  Dell <- solve(data,...)
+  z$setinverse(Dell)
+  Dell
+} 
